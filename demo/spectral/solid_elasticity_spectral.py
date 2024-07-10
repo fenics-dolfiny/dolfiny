6159308daf1227500434b8bb850e5c35d83fe85a
Andreas Zilian <andreas.zilian@uni.lu>
2024-07-10 09:49:27 +0000

    Adjust extra compile flags for arm64

--- demo/spectral/solid_elasticity_spectral.py
+++ demo/spectral/solid_elasticity_spectral.py
@@ -132,7 +132,7 @@ opts["pc_type"] = "cholesky"
 opts["pc_factor_mat_solver_type"] = "mumps"
 
 # FFCx options
-jit_options = dict(cffi_extra_compile_args=["-O3", "-fno-var-tracking"])  # may take several minutes
+jit_options = dict(cffi_extra_compile_args=["-std=gnu17", "-g0"])  # may take several minutes
 
 # Create nonlinear problem: SNES
 problem = dolfiny.snesblockproblem.SNESBlockProblem(forms, m, prefix=name, jit_options=jit_options)
