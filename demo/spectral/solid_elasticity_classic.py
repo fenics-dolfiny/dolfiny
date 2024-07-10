787e16e41b4a3408d092e8fe7a5e3e78e5aa412e
Andreas Zilian <andreas.zilian@uni.lu>
2024-07-10 10:23:12 +0000

    Use non-empty flag

--- demo/spectral/solid_elasticity_classic.py
+++ demo/spectral/solid_elasticity_classic.py
@@ -132,7 +132,7 @@ opts["pc_type"] = "cholesky"
 opts["pc_factor_mat_solver_type"] = "mumps"
 
 # FFCx options
-jit_options = dict(cffi_extra_compile_args=[""])
+jit_options = dict(cffi_extra_compile_args=["-g0"])
 
 # Create nonlinear problem: SNES
 problem = dolfiny.snesblockproblem.SNESBlockProblem(forms, m, prefix=name, jit_options=jit_options)
