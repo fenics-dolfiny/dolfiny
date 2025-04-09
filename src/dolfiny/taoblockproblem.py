381425c5860ae7b6a493d9667869a01d4ccfe261
Paul T. Kühner <paul.kuehner@uni.lu>
2025-04-09 09:35:17 +0000

    Add TAO problem solver parallel ready (non-blocked)

--- src/dolfiny/taoblockproblem.py
+++ src/dolfiny/taoblockproblem.py
@@ -1,18 +1,139 @@
-from dolfiny.localsolver import LocalSolver
+from collections.abc import Sequence
+
+from mpi4py import MPI
+from petsc4py import PETSc
+
+import dolfinx
+import dolfinx.fem.petsc
+
+from dolfiny.function import functions_to_vec, vec_to_functions
 
 
 class TAOBlockProblem:
     def __init__(
         self,
-        F,
+        F: Sequence[dolfinx.fem.Form],
         u: list,
-        bcs=[],
-        J_form=None,
-        nest=False,
-        restriction=None,
+        bcs: Sequence[dolfinx.fem.DirichletBC] = [],
+        J: Sequence[dolfinx.fem.Form] | None = None,
+        H: Sequence[dolfinx.fem.Form] | None = None,
         prefix=None,
-        localsolver: LocalSolver = None,
+        # nest=False,
         form_compiler_options: dict | None = None,
         jit_options: dict | None = None,
     ):
-        pass
+        if len(F) == 0:
+            raise RuntimeError("List of provided residual forms is empty!")
+
+        if len(u) == 0:
+            raise RuntimeError("List of provided solution functions is empty!")
+
+        if not all(isinstance(_u, dolfinx.fem.Function) for _u in u):
+            raise RuntimeError("Provided solution function not of type dolfinx.Function!")
+
+        self._F = F
+        self._J = J
+        self._H = H
+        self._u = u
+        self._bcs = bcs
+
+        # TODO: check for eq/congruent comms
+        self._comm = self._u[0].function_space.mesh.comm
+
+        if self._J is None:
+            raise NotImplementedError()
+
+        if self._H is None:
+            raise NotImplementedError()
+
+        self.F_form_all_ufc = dolfinx.fem.form(
+            self._F, form_compiler_options=form_compiler_options, jit_options=jit_options
+        )
+
+        self.J_form_all_ufc = dolfinx.fem.form(
+            self._J, form_compiler_options=form_compiler_options, jit_options=jit_options
+        )
+
+        self.H_form_all_ufc = dolfinx.fem.form(
+            self._H, form_compiler_options=form_compiler_options, jit_options=jit_options
+        )
+
+        # By default, copy all forms as the forms used in assemblers
+        self._F = self.F_form_all_ufc.copy()
+        self._J = self.J_form_all_ufc.copy()
+        self._H = self.H_form_all_ufc.copy()
+
+        # TOOD: remove
+        if self._H is None:
+            raise NotImplementedError()
+
+        self._x0 = self._u[0].x.petsc_vec.copy()
+        self._tao = PETSc.TAO().create(self._comm)  # type: ignore
+        self._tao.setOptionsPrefix(prefix)
+        self._tao.setFromOptions()
+        self._tao.setObjective(self._F_callback)
+        self._tao.setGradient(self._J_callback, self._u[0].x.petsc_vec.copy())
+        self._tao.setHessian(self._H_callback, dolfinx.fem.petsc.create_matrix(self._H[0]))
+
+    def _F_callback(self, tao, x):
+        vec_to_functions(x, self._u)
+
+        assert len(self._F) == 1  # TODO: extend for blocked problems
+
+        local_F = dolfinx.fem.assemble_scalar(self._F[0])
+        F_value = self._comm.allreduce(local_F, op=MPI.SUM)
+        return F_value
+
+    def _J_callback(self, tao, x, J):
+        J.zeroEntries()
+        J.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
+
+        vec_to_functions(x, self._u)
+
+        dolfinx.fem.petsc.assemble_vector(J, self._J[0])
+        J.ghostUpdate(addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
+
+        dolfinx.fem.petsc.apply_lifting(
+            J,
+            self._H,
+            bcs=dolfinx.fem.bcs_by_block(
+                dolfinx.fem.extract_function_spaces([self._H], 1), self._bcs
+            ),
+            x0=[x],
+            alpha=-1.0,
+        )
+        dolfinx.fem.petsc.set_bc(
+            J,
+            bcs=dolfinx.fem.bcs_by_block(dolfinx.fem.extract_function_spaces(self._J), self._bcs)[
+                0
+            ],
+            x0=x,
+            alpha=-1.0,
+        )
+
+    def _H_callback(self, tao, x, H, P):
+        H.zeroEntries()
+        vec_to_functions(x, self._u)
+
+        dolfinx.fem.petsc.assemble_matrix(H, [self._H], self._bcs, diag=1.0)
+
+        H.assemble()
+
+        if P != H:
+            H.copy(P)
+            P.assemble()
+
+    def solve(self, u_init=None):
+        if u_init is not None:
+            functions_to_vec(u_init, self._x0)
+
+        # TODO: monitor
+
+        self._tao.solve(self._x0)
+        ls = self._tao.getLineSearch()
+        self._tao.view()
+        ls.view()
+
+        solution = [dolfinx.fem.Function(u.function_space, name=u.name) for u in self._u]
+        vec_to_functions(self._x0, solution)
+        return solution
--- test/unit/test_taoproblem.py
+++ test/unit/test_taoproblem.py
@@ -1,25 +1,74 @@
+from mpi4py import MPI
 from petsc4py import PETSc
 
-import basix
 import dolfinx
 import ufl
 
 import numpy as np
-import pytest
 
 import dolfiny
+import dolfiny.taoblockproblem
+
+
+def L2_norm(u, comm) -> float:
+    form = dolfinx.fem.form(ufl.inner(u, u) * ufl.dx)
+    local_squared = dolfinx.fem.assemble_scalar(form)
+    global_squared = comm.allreduce(local_squared, op=MPI.SUM)
+    return np.sqrt(global_squared)
 
 
 def test_poisson(V1):
     W = V1
 
-    u = dolfinx.fem.Function(W)
-    f = 1.0
+    # TODO: return to smaller mesh at some point
+    n = 32
+    mesh = dolfinx.mesh.create_unit_square(
+        MPI.COMM_WORLD, n, n, ghost_mode=dolfinx.mesh.GhostMode.shared_facet
+    )
+    W = dolfinx.fem.functionspace(mesh, ("P", 1))
 
-    F = 1/2 * ufl.inner(ufl.grad(u), ufl.grad(u)) * ufl.dx - ufl.inner(f, u) * ufl.dx
+    u = dolfinx.fem.Function(W)
+    f = -1.0
+
+    F = 1 / 2 * ufl.inner(ufl.grad(u), ufl.grad(u)) * ufl.dx + ufl.inner(f, u) * ufl.dx
+    J = ufl.derivative(F, u, ufl.TestFunction(W))
+    H = ufl.derivative(J, u, ufl.TrialFunction(W))
+
+    top = W.mesh.topology
+    top.create_connectivity(top.dim - 1, top.dim)
+    boundary_facets = dolfinx.mesh.exterior_facet_indices(top)
+    boundary_dofs = dolfinx.fem.locate_dofs_topological(W, top.dim - 1, boundary_facets)
+    bc = dolfinx.fem.dirichletbc(dolfinx.fem.Constant(W.mesh, 0.0), boundary_dofs, W)
 
     opts = PETSc.Options("poisson")
-    opts.setValue("tao_type", "nls")
+    opts["tao_type"] = "nls"
+    opts["tao_gatol"] = 1e-12
+    opts["tao_grtol"] = 0
+    opts["tao_gttol"] = 0
+    opts["tao_nls_ksp_type"] = "preonly"
+    opts["tao_nls_pc_type"] = "cholesky"
+    opts["tao_nls_pc_factor_mat_solver_type"] = "mumps"
+    # opts["tao_monitor"] = ""
+    # opts["tao_ls_monitor"] = ""
 
-    problem = dolfiny.snesblockproblem.TAOBlockProblem(F, [u], prefix="monolithic")
-    (sol,) = problem.solve()
+    problem = dolfiny.taoblockproblem.TAOBlockProblem(
+        [F], [u], bcs=[bc], J=[J], H=[H], prefix="poisson"
+    )
+    (sol_optimization,) = problem.solve()
+
+    weak_form = ufl.replace(J, {u: ufl.TrialFunction(W)})
+    a, L = ufl.lhs(weak_form), ufl.rhs(weak_form)
+
+    problem = dolfinx.fem.petsc.LinearProblem(
+        a, L, bcs=[bc], petsc_options={"ksp_type": "preonly", "pc_type": "lu"}
+    )
+    sol_weak_form = problem.solve()
+
+    norm = L2_norm(sol_optimization - sol_weak_form, MPI.COMM_WORLD)
+
+    assert np.allclose(norm, 0)
+
+    # with dolfinx.io.VTXWriter(W.mesh.comm, "opt.bp", u, "bp4") as file:
+    #     file.write(0.0)
+    # with dolfinx.io.VTXWriter(W.mesh.comm, "direct.bp", sol_weak_form, "bp4") as file:
+    #     file.write(0.0)
