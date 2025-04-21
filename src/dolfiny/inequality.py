bfe73a639ec8b2a701defdee3a19a8e4442b7f01
Paul T. Kühner <paul.kuehner@uni.lu>
2025-04-21 22:05:18 +0200

    Introduce ufl extension Inequality

---
+++ src/dolfiny/inequality.py
@@ -0,0 +1,48 @@
+import ufl
+
+
+class Inequality:
+    """Stores an inequality as
+
+        lhs <= rhs
+
+    where lhs is a form and rhs a constant
+    """
+
+    def __init__(self, lhs: ufl.Form, rhs: int | float) -> None:
+        self._lhs = lhs
+        self._rhs = rhs
+
+    def __str__(self):
+        return f"{self._lhs.__str__()} <= {self._rhs.__str__()}"
+
+    @property
+    def lhs(self) -> ufl.Form:
+        return self._lhs
+
+    @property
+    def rhs(self) -> int | float:
+        return self._rhs
+
+    def __eq__(self, other: object) -> bool:
+        return (
+            isinstance(other, Inequality)
+            and self.lhs.equals(other.lhs)
+            and self.rhs.equals(other.rhs)
+        )
+
+
+# Overwrite <=, >= operations for ufl.Form
+
+
+def custom_le(form: ufl.Form, other: int | float) -> Inequality:
+    return Inequality(form, other)
+
+
+def custom_ge(form: ufl.Form, other: int | float) -> Inequality:
+    """form >= other iff. -form <= - other"""
+    return Inequality(-form, -other)
+
+
+ufl.Form.__le__ = custom_le
+ufl.Form.__ge__ = custom_ge
---
+++ test/unit/test_inequality.py
@@ -0,0 +1,57 @@
+import dolfinx
+import ufl
+import ufl.constantvalue
+
+import pytest
+
+from dolfiny.inequality import Inequality
+
+forms = [
+    lambda V: dolfinx.fem.Constant(V.mesh, 1.0) * ufl.dx,
+    lambda V: dolfinx.fem.Function(V) ** 2 * ufl.dx,
+    lambda V: ufl.inner(dolfinx.fem.Function(V), ufl.TestFunction(V)) * ufl.dx,
+    lambda V: ufl.inner(ufl.TestFunction(V), ufl.TrialFunction(V)) * ufl.dx,
+]
+
+
+@pytest.mark.parametrize("a", forms)
+@pytest.mark.parametrize("b", [1.0, 1])
+def test_equality(V1, a, b):
+    a = a(V1)
+
+    inequality = a == b
+    assert isinstance(inequality, ufl.equation.Equation)
+    assert inequality.lhs == a
+    assert inequality.rhs == b
+
+    inequality = b == a
+    assert isinstance(inequality, ufl.equation.Equation)
+    assert inequality.lhs == a
+    assert inequality.rhs == b
+
+
+@pytest.mark.parametrize("a", forms)
+@pytest.mark.parametrize("b", [1.0, 1])
+def test_inequality(V1, a, b):
+    a = a(V1)
+
+    inequality = a <= b
+    assert isinstance(inequality, Inequality)
+    assert inequality.lhs == a
+    assert inequality.rhs == b
+    assert str(inequality) == f"{a} <= {b}"
+
+    inequality = b <= a
+    assert isinstance(inequality, Inequality)
+    assert inequality.lhs == -a
+    assert inequality.rhs == -b
+
+    inequality = a >= b
+    assert isinstance(inequality, Inequality)
+    assert inequality.lhs == -a
+    assert inequality.rhs == -b
+
+    inequality = b >= a
+    assert isinstance(inequality, Inequality)
+    assert inequality.lhs == a
+    assert inequality.rhs == b
