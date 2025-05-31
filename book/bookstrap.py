4c3f39ea3af15beccb747b883ee1514e90221356
Michal Habera <michal.habera@gmail.com>
2025-05-31 18:29:26 +0000

    Add nodejs&npm to docker, remove BASE_URL

--- book/bookstrap.py
+++ book/bookstrap.py
@@ -1,23 +1,11 @@
-import argparse
 import os
 import pathlib
 import subprocess
 
-system_dependencies = ["nodejs", "npm"]
 demo_files = ["obstacle/membrane.py"]  # relative from demo/
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--install-dependencies", action="store_true", default=False)
-args = parser.parse_args()
 
 if os.getcwd() != os.path.dirname(os.path.abspath(__file__)):
     raise RuntimeError("setup.py expects to be executed from the book/ directory.")
-
-if args.install_dependencies:
-    print("Installing dependencies...")
-    subprocess.run(["apt-get", "-qq", "update"], check=True)
-    subprocess.run(["apt-get", "-y", "install", *system_dependencies], check=True)
 
 
 for demo in demo_files:
