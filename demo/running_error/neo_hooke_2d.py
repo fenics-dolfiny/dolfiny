ab9d2f548aefe94e029f6fd7da34008ed6c7453f
paul.kuehner <paul.kuehner@uni.lu>
2026-08-08 13:53:02 +0200

    Move Matteo author definition to central location (adapt style)

--- demo/running_error/neo_hooke_2d.py
+++ demo/running_error/neo_hooke_2d.py
@@ -4,12 +4,7 @@
 #   - az
 #   - mh
 #   - ptk
-#   - name: Matteo Croci
-#     email: mcroci@bcamath.org
-#     affiliations:
-#       - id: bcam
-#         institution: BCAM - Basque Center for Applied Mathematics
-#     url: https://croci.github.io/
+#   - mc
 # ---
 
 # %% [markdown]
